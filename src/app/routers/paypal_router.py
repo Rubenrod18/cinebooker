918c80dd6668f09faff1a7dde68e8dc49081513e
Rubén Rodríguez Ramírez <rubenrod.018@gmail.com>
2025-11-25 13:31:20 +0100

    feat(emails): send an email to the customer when the payment is paid (#76)
    * feat(emails): send an email to the customer when the payment is paid
    
    * ci: add mailpit on integrity checks

--- src/app/routers/paypal_router.py
+++ src/app/routers/paypal_router.py
@@ -1,8 +1,13 @@
+import base64
 import logging
+from io import BytesIO
 from typing import Annotated
 
+import qrcode
 from dependency_injector.wiring import inject, Provide
 from fastapi import APIRouter, Depends, Header, HTTPException
+from fastapi_mail import MessageSchema, MessageType
+from jinja2 import Environment, FileSystemLoader
 from sqlalchemy.orm import Session
 from starlette import status
 from starlette.requests import Request
@@ -14,6 +19,8 @@ from app.schemas import payment_schemas, paypal_schemas
 from app.services.booking_service import BookingService
 from app.services.invoice_service import InvoiceService
 from app.services.payment_service import PaymentService
+from app.utils.constants import DEFAULT_DATETIME_FORMAT
+from extensions import get_fastapi_mail
 
 router = APIRouter(prefix='/paypal', tags=['paypal'])
 logger = logging.getLogger(__name__)
@@ -69,6 +76,56 @@ def create_payment_route(
     return paypal_schemas.CreatePaymentResponseSchema(order_id=order['id'], approve_link=approve_link)
 
 
+async def generate_qr_base64(data: str) -> str:
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color='black', back_color='white')
+
+    buffer = BytesIO()
+    img.save(buffer)
+    qr_bytes = buffer.getvalue()
+    return base64.b64encode(qr_bytes).decode('utf-8')
+
+
+# HACK: Move this logic to a Celery task
+async def send_email_to_customer(fastapi_mail, payment):
+    booking = payment.booking
+    ticket_data = [
+        {
+            'qr_base64': f'data:image/png;base64,{await generate_qr_base64(booking_seat.ticket.barcode_value)}',
+            'row': booking_seat.seat.row,
+            'number': booking_seat.seat.number,
+            'barcode_value': booking_seat.ticket.barcode_value,
+        }
+        for booking_seat in booking.booking_seats
+    ]
+    env = Environment(loader=FileSystemLoader('templates/mails'))
+    template = env.get_template('tickets.html')
+    data = {
+        'movie_title': booking.showtime.movie.title,
+        'show_datetime': booking.showtime.start_time.strftime(DEFAULT_DATETIME_FORMAT),
+        'screen_name': booking.showtime.screen.name,
+        'payment_datetime': payment.created_at.strftime(DEFAULT_DATETIME_FORMAT),
+        'booking_id': booking.id.hex,
+        'tickets': ticket_data,
+    }
+    rendered_html = template.render(**data)
+    message = MessageSchema(
+        subject='FastAPI-Mail module',
+        recipients=['hello_world@mail.com'],
+        body=rendered_html,
+        subtype=MessageType.html,
+    )
+    await fastapi_mail.send_message(message)
+
+
 @router.post('/webhook', status_code=status.HTTP_204_NO_CONTENT)
 @inject
 async def paypal_webhook(
@@ -82,6 +139,7 @@ async def paypal_webhook(
     paypal_cert_url: str | None = Header(None, alias='PAYPAL-CERT-URL'),
     paypal_auth_algo: str | None = Header(None, alias='PAYPAL-AUTH-ALGO'),
     paypal_transmission_sig: str | None = Header(None, alias='PAYPAL-TRANSMISSION-SIG'),
+    fastapi_mail=Depends(get_fastapi_mail),
 ) -> None:
     """Handle PayPal webhooks.
 
@@ -128,6 +186,7 @@ async def paypal_webhook(
             booking_service.confirmed(payment.booking)
             invoice_service.paid(payment.booking.invoice)
             logger.info('✅ Payment succeeded!')
+            await send_email_to_customer(fastapi_mail, payment)
         else:
             payment_service.cancelled(payment, **{'provider_metadata': resource})
             booking_service.cancelled(payment.booking)
