d1801fa366ea959fbd9adf246f2b602b68e22d1d
Rubén Rodríguez Ramírez <rubenrod.018@gmail.com>
2025-11-25 10:10:51 +0100

    feat: add integration with fastapi-mail python package (#75)

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ import os
 from decimal import Decimal
 from functools import lru_cache
 
-from dotenv import load_dotenv
+from dotenv import find_dotenv, load_dotenv
 from pydantic_settings import BaseSettings
 
 load_dotenv()
@@ -25,6 +25,18 @@ class BaseConfig(BaseSettings):
     PAYPAL_WEBHOOK_ID: str = os.getenv('PAYPAL_WEBHOOK_ID')
     PAYPAL_SANDBOX: bool = os.getenv('PAYPAL_SANDBOX')
 
+    # FastAPI-Mail
+    MAIL_USERNAME: str = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD: str = os.getenv('MAIL_PASSWORD')
+    MAIL_FROM: str = os.getenv('MAIL_FROM')
+    MAIL_PORT: int = os.getenv('MAIL_PORT')
+    MAIL_SERVER: str = os.getenv('MAIL_SERVER')
+    MAIL_FROM_NAME: str = os.getenv('MAIL_FROM_NAME')
+    MAIL_STARTTLS: bool = os.getenv('MAIL_STARTTLS')
+    MAIL_SSL_TLS: bool = os.getenv('MAIL_SSL_TLS')
+    USE_CREDENTIALS: bool = os.getenv('USE_CREDENTIALS')
+    VALIDATE_CERTS: bool = os.getenv('VALIDATE_CERTS')
+
     # Mr Developer
     DEFAULT_VAT_RATE: Decimal = Decimal(os.getenv('DEFAULT_VAT_RATE', Decimal('0.21')))
     BARCODE_LENGTH: int = int(os.getenv('BARCODE_LENGTH', 30))
@@ -44,6 +56,9 @@ class DevConfig(BaseConfig):
 class TestConfig(BaseConfig):
     DEBUG: bool = True
 
+    class Config:
+        env_file = '.env.test'
+
 
 @lru_cache
 def get_settings():
@@ -52,6 +67,8 @@ def get_settings():
     if env == 'production':
         return ProdConfig()
     elif env == 'testing':
+        # HACK: It's not the best way to import test environment values
+        load_dotenv(find_dotenv('.env.test'), override=True)
         return TestConfig()
 
     return DevConfig()
